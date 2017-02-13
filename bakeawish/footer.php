b6e471c1a6c854abd2a87e1fe43d267d5bc2369a
Lauren Abele <lauren.abele@c02rr1n6g8wp.bancvue.com>
2017-02-13 13:49:55 -0600

    YO HERE WE GO

---
+++ bakeawish/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Bake_A_Wish
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bakeawish' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bakeawish' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'bakeawish' ), 'bakeawish', '<a href="https://automattic.com/" rel="designer">Kasasa FEDLab</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
