bba8dfe43d06d699533390354265a074cb72a5bd
Lauren Abele <lauren.abele@c02rr1n6g8wp.bancvue.com>
2017-02-14 13:12:55 -0600

    Adding Bootstrap css and js, plus enqueued in functions.php

--- bakeawish/functions.php
+++ bakeawish/functions.php
@@ -111,6 +111,14 @@ function bakeawish_scripts() {
 
 	wp_enqueue_script( 'bakeawish-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+	wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/bootstrap-3.3.7-dist/js/bootstrap.min.js', array( 'jquery' ), '3.0.1', true );
+
+	wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/bootstrap-3.3.7-dist/css/bootstrap.min.css', array(), '3.0.1', 'all' );
+
+	wp_enqueue_script( 'bootstrap-js' );
+
+	wp_enqueue_style( 'bootstrap-css' );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
