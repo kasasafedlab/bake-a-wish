b276d9b4a3dad24fa0715fb8828b76e2cd505552
Scott <rswatx@gmx.com>
2017-02-17 14:02:17 -0600

    a comment

---
+++ bakeawish/wp-content/themes/bakeawish/template-gallery.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template Name: Gallery
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Bake_A_Wish
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				 
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
