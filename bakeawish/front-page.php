bb1967e7c931363236653ed29b233af6e2c65bea
Megan King <mgnking12@gmail.com>
2017-02-17 13:37:27 -0600

    home page

--- bakeawish/front-page.php
+++ bakeawish/front-page.php
@@ -21,28 +21,40 @@ get_header(); ?>
 			while ( have_posts() ) : the_post();
 
 				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				// if ( comments_open() || get_comments_number() ) :
-				// 	comments_template();
-				// endif;
-
-
+				
 				$hero = get_field('home_page_hero_image');
-			             $bannerText = get_field('home_page_banner_text');
+			             $bannerTextSmall = get_field('home_page_banner_small_text');
+			             $bannerTextLarge = get_field('home_page_banner_large_text');
 			             $fpImage1 = get_field('home_page_fp_image1');
-			             $fpImage1 = get_field('home_page_fp_content1');
+			             $fpContent1 = get_field('home_page_fp_content1');
 			             $fpImage2 = get_field('home_page_fp_image2');
-			             $fpImage1 = get_field('home_page_fp_content2');
+			             $fpContent2 = get_field('home_page_fp_content2');
 			             $fpImage3  = get_field('home_page_fp_image3');
-			             $fpImage1 = get_field('home_page_fp_content3');
+			             $fpContent3 = get_field('home_page_fp_content3');
 
 			endwhile; // End of the loop.
 			?>
 			<div class="row">
 			<div class="col-lg-12">
 				<img class="heroImage image-responsive" src="<?php echo $hero; ?>" />
-				<div class="text-center" id="banner-text"><?php echo $bannerText; ?>  </div>
+				<div class="text-center" id="banner-text">
+				<p><?php echo $bannerTextSmall; ?></p>
+				<h2><?php echo $bannerTextLarge; ?></h2>
+				</div>
+
+				<div class="row">
+					<div class="col-lg-10 col-md-offset-1">
+						<div class="col-lg-4">
+							<img class="col-lg-6 col-md-offset-3  image-responsive" src="<?php echo $fpImage1; ?>" />
+						</div>
+						<div class="col-lg-4">
+							<img class="col-lg-6 col-md-offset-3 image-responsive" src="<?php echo $fpImage2; ?>" />
+						</div>
+						<div class="col-lg-4">
+							<img class="col-lg-6 col-md-offset-3 image-responsive" src="<?php echo $fpImage3; ?>" />
+						</div>
+					</div>
+				</div>
 			</div>
 			</div>
 			
