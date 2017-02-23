42fe152ab468ed28d5d20b70f17a7172e8e8fd2a
Lauren Abele <lauren.abele@c02rr1n6g8wp.bancvue.com>
2017-02-23 08:35:17 -0600

    Adding About page styles for posts carousel

--- bakeawish/template-laurens.php
+++ bakeawish/template-laurens.php
@@ -19,14 +19,29 @@ get_header(); ?>
 
 		<?php
 			while ( have_posts() ) : the_post(); ?>
+			<span id=prev>&lt;&lt;Prev </span>
+        <span id=next> Next&gt;&gt;</span>
 			<ul class="recent-posts">
-			<?php $the_query = new WP_Query( 'posts_per_page=5' ); 
+			
+			 <div class="cycle-slideshow composite-example" data-cycle-fx="carousel" 
+    data-cycle-slides="> div"
+    data-cycle-timeout="4000"
+    data-cycle-prev="#prev"
+    data-cycle-next="#next"
+    data-cycle-carousel-visible="3"
+    >
+
+<?php $the_query = new WP_Query( 'posts_per_page=5' ); 
 			 while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
-			<li class="col-sm-4">
-				<h4><?php the_title(); ?></h4>
-				<p><?php the_excerpt(); ?></p>
-				<a href="<?php the_excerpt(); ?>">Learn More</a>
-			</li>
+			<div class="col-sm-4 recent-post-slide">
+				<a href="<?php the_permalink(); ?>">
+					<div class="recent-posts-container">
+						<h4><?php the_title(); ?></h4>
+						<?php the_excerpt(); ?>
+						<span>Learn More</span>
+					</div><!-- .recent-posts-container-->	
+				</a>	
+			</div>
 
 
 
@@ -45,6 +60,9 @@ get_header(); ?>
 
 			?>
 
+</div>
+<!-- empty element for pager links -->
+    <div class="cycle-pager"></div>
 </ul>
 
 
@@ -53,6 +71,12 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+<script>
+$(document).ready( function() {	
 
+	var n = $('.recent-post-slide').length;
+	console.log(n);
+	});
+</script>
 <?php
 get_footer();
