7109718e40cec99fbad74060d3fb1d9ce0f27992
Mason Posch <masonposch@gmail.com>
2017-02-28 13:57:38 -0600

    adding if statements to Who We serve

--- bakeawish/template-who-we-serve.php
+++ bakeawish/template-who-we-serve.php
@@ -59,60 +59,66 @@ get_header(); ?>
 			<div class="who-we-are row">
 
 				<div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1 about">
-					<p><?php echo $whoWeServeDescription; ?><p>
-					
+					<?php if( $whoWeServeDescription ): ?>
+						<p><?php echo $whoWeServeDescription; ?><p>
+					<?php endif; ?>
 				</div>
 
 			</div>
 
 
+			<?php if( $partnerDescription ): ?>
+				<div class="row">
+					<div class="who-we-are col-lg-12">
 
-			<div class="row">
-				<div class="who-we-are col-lg-12">
+						<h2 class="text-center">Featured Partners</h2>
 
-					<h2 class="text-center">Featured Partner</h2>
-
-				</div>
-			</div><!-- .row -->	
-
-
-			<!--PARTNERS ARE LISTED HERE-->
-			<div class="row">
-				<span class="partner"><!--partner box-->
-					<div id="partnerLogo" class="partnerLogo col-lg-4 col-lg-offset-2 col-md-4 col-md-offset-2 col-sm-5 col-sm-offset-1 col-xs-5 col-xs-offset-1" style="background:<?php echo $partnerLogoBackground; ?>;">
-						<img class="col-xs-10 col-xs-offset-1" src="<?php echo $partnerLogo; ?>" />
 					</div>
-					<div id="partnerDescription" class="partnerDescription col-lg-4 col-md-4 col-sm-5 col-xs-5" style="background: #f3f3f3;">
-						<p><?php echo $partnerDescription; ?></p>
-						<h3><a href="<?php echo $partnerWebsite; ?>" target="_blank">VISIT THEIR WEBSITE</a></h3>
-					</div>	
-				</span>
-			</div><!--End Row-->
-			<div class="row">
-				<span class="partner"><!--partner box-->
-					<div id="partnerLogo" class="partnerLogo col-lg-4 col-lg-offset-2 col-md-4 col-md-offset-2 col-sm-5 col-sm-offset-1 col-xs-5 col-xs-offset-1" style="background:<?php echo $partnerLogoBackground2; ?>;">
-						<img class="col-xs-10 col-xs-offset-1" src="<?php echo $partnerLogo2; ?>" />
+				</div><!-- .row -->	
+
+
+				<!--PARTNERS ARE LISTED HERE-->
+				<div class="row">
+					<span class="partner"><!--partner box-->
+						<div id="partnerLogo" class="partnerLogo col-lg-4 col-lg-offset-2 col-md-4 col-md-offset-2 col-sm-5 col-sm-offset-1 col-xs-5 col-xs-offset-1" style="background:<?php echo $partnerLogoBackground; ?>;">
+							<img class="col-xs-10 col-xs-offset-1" src="<?php echo $partnerLogo; ?>" />
+						</div>
+						<div id="partnerDescription" class="partnerDescription col-lg-4 col-md-4 col-sm-5 col-xs-5" style="background: #f3f3f3;">
+							<p><?php echo $partnerDescription; ?></p>
+							<h3><a href="<?php echo $partnerWebsite; ?>" target="_blank">VISIT THEIR WEBSITE</a></h3>
+						</div>	
+					</span>
+				</div><!--End Row-->
+				<div class="row">
+					<span class="partner"><!--partner box-->
+						<div id="partnerLogo" class="partnerLogo col-lg-4 col-lg-offset-2 col-md-4 col-md-offset-2 col-sm-5 col-sm-offset-1 col-xs-5 col-xs-offset-1" style="background:<?php echo $partnerLogoBackground2; ?>;">
+							<img class="col-xs-10 col-xs-offset-1" src="<?php echo $partnerLogo2; ?>" />
+						</div>
+						<div id="partnerDescription" class="partnerDescription col-lg-4 col-md-4 col-sm-5 col-xs-5" style="background: #f3f3f3;">
+							<p><?php echo $partnerDescription2; ?></p>
+							<h3><a href="<?php echo $partnerWebsite2; ?>" target="_blank">VISIT THEIR WEBSITE</a></h3>
+						</div>	
+					</span>
+				</div><!--End Row-->
+			<?php endif; ?>
+
+
+			<?php if( $thoseWeveServed ): ?>
+				
+				<div class="row">
+					<div class="who-we-are col-lg-12">
+
+						<h2 class="text-center">Those We've Served</h2>
+
 					</div>
-					<div id="partnerDescription" class="partnerDescription col-lg-4 col-md-4 col-sm-5 col-xs-5" style="background: #f3f3f3;">
-						<p><?php echo $partnerDescription2; ?></p>
-						<h3><a href="<?php echo $partnerWebsite2; ?>" target="_blank">VISIT THEIR WEBSITE</a></h3>
-					</div>	
-				</span>
-			</div><!--End Row-->
+				</div><!-- .row -->	
 
 
-			<div class="row">
-				<div class="who-we-are col-lg-12">
+				<div class="row thoseWeveServed">
+					<?php echo $thoseWeveServed; ?>
+				</div><!-- .row -->	
 
-					<h2 class="text-center">Those We've Served</h2>
-
-				</div>
-			</div><!-- .row -->	
-
-
-			<div class="row thoseWeveServed">
-				<?php echo $thoseWeveServed; ?>
-			</div><!-- .row -->	
+			<?php endif; ?>
 
 			
 			<!-- <div class="row-fluid"> -->
